3fa8d0d5c0bc9c1eac57dec0d038c906e3933402
Salami Azeez <salamitopeazeez@gmail.com>
2025-08-14 01:57:36 +0100

    Added ButtonProps interface

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,14 @@
 
-export const button = () => {
+interface ButtonProps {
+  buttonText?: string;
+  className?: string;
+  href?: string;
+  variant: "primary" | "secondary";
+  onClick?: () => void;
+  children?: React.ReactNode;
+}
+
+export const Button = () => {
   return (
     <div>button</div>
   )
